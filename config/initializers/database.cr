8257695b1ce40e4ac547b7408dda7b7f7c562a5f
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-06-22 12:56:58 -0300

    chore(config/initializers/database.cr): Configuração da conexão com banco de dados do "Jennifer ORM"

---
+++ config/initializers/database.cr
@@ -0,0 +1,13 @@
+require "jennifer"
+require "jennifer/adapter/mysql"    # for MySQL
+require "jennifer/adapter/postgres" # for PostgreSQL
+
+APP_ENV = ENV["APP_ENV"]? || "development"
+
+Jennifer::Config.configure do |conf|
+  conf.read("config/database.yml", APP_ENV)
+  conf.from_uri(ENV["DATABASE_URI"]) if ENV.has_key?("DATABASE_URI")
+  conf.logger.level = APP_ENV == "development" ? Log::Severity::Debug : Log::Severity::Error
+end
+
+Log.setup "db", Log::Severity::Debug, Log::IOBackend.new(formatter: Jennifer::Adapter::DBFormatter)
