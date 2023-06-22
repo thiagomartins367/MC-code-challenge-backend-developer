ac946e4523166dad180078a521226feba4fe8bdf
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-06-22 13:45:58 -0300

    refactor(src/api.cr): Configurações do "jennifer" importadas e contexto "env" renomeado para "context" na rota GET "/"

--- src/api.cr
+++ src/api.cr
@@ -1,20 +1,21 @@
 require "kemal"
 require "json"
+require "../config/config"
 
 # TODO: Write documentation for `Api`
 module Api
   VERSION = "0.1.0"
 
   # TODO: Put your code here
-  get "/" do |env|
+  get "/" do |context|
     begin
       file_path = "./public/docs/index.html"
       file_content = File.read(file_path)
-      env.response.headers["Content-Type"] = "text/html"
-      env.response.print file_content
+      context.response.headers["Content-Type"] = "text/html"
+      context.response.print file_content
     rescue ex : File::Error
-      env.response.headers["Content-Type"] = "text/plain"
-      halt env, status_code: 404, response: "Document not found!"
+      context.response.headers["Content-Type"] = "text/plain"
+      halt context, status_code: 404, response: "Document not found!"
     end
   end
 
