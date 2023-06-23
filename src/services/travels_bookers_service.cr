a4e1824bc773478b39c5337a811b82946259e72e
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-06-22 23:25:42 -0300

    feat(src/services/travels_bookers_service.cr): Métodos "get_all_travels_booker" e "format_travels_bookers" implementados

--- src/services/travels_bookers_service.cr
+++ src/services/travels_bookers_service.cr
@@ -2,13 +2,33 @@ require "../repository/travels_bookers_repository"
 
 module Api
   class TravelsBookerService
-    def self.create_travels_booker(request_body)
+    def initialize(@repository : TravelsBookerRepository)
+    end
+
+    getter :repository
+
+    private def format_travels_bookers(travels_bookers)
+      travels_bookers.map do |travels_booker|
+        travel_stops : Array(String) = travels_booker.travel_stops.split(",")
+        {
+          id:           travels_booker.id,
+          travel_stops: travel_stops.map { |location| location.to_i32 },
+        }
+      end
+    end
+
+    def create_travels_booker(request_body)
       travel_stops = request_body["travel_stops"].as(Array)
       travel_stops_string = travel_stops.join(",")
-      new_travel_stops = TravelsBookerRepository.create_travels_booker(travel_stops_string)
-      new_travel_stops_array = new_travel_stops["travel_stops"].as_s.split(",")
-      new_travel_stops_formatted = new_travel_stops_array.map { |location| location.to_i32 }
-      {id: new_travel_stops["id"], travel_stops: new_travel_stops_formatted}
+      new_travels_booker = repository.create_travels_booker(travel_stops_string)
+      new_travels_booker_formatted = format_travels_bookers([new_travels_booker])
+      new_travels_booker_formatted[0]
+    end
+
+    def get_all_travels_booker
+      travels_bookers = repository.get_all_travels_booker
+      travels_booker_formatted = format_travels_bookers(travels_bookers.to_a)
+      travels_booker_formatted
     end
   end
 end
