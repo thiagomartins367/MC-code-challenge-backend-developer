88c3fb4fd804b6051b82c40497a854708bca2b76
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-06-22 23:24:32 -0300

    feat(src/controllers/travels_bookers_controller.cr): Rota GET "/travel_plans" implementada e filter "before_all" das rotas "/travel_plans" implementado
    Outras modificações: refatoração nas rotas e método "entity_factory" implementado

--- src/controllers/travels_bookers_controller.cr
+++ src/controllers/travels_bookers_controller.cr
@@ -1,13 +1,48 @@
 require "../services/travels_bookers_service"
+require "../repository/travels_bookers_repository"
 
 module Api
   class TravelsBookerController
+    def initialize(@service = TravelsBookerService)
+    end
+
+    getter :service
+
+    def self.entity_factory
+      repository = TravelsBookerRepository.new
+      service = TravelsBookerService.new(repository)
+      service
+    end
+
+    before_all "/travel_plans" do |context|
+      response = context.response
+      response.headers["Content-Type"] = "application/json"
+    end
+
     post "/travel_plans" do |context|
       response = context.response
       params = context.params
-      result = TravelsBookerService.create_travels_booker(params.json)
-      response.headers["Content-Type"] = "application/json"
+      result = entity_factory.create_travels_booker(params.json)
+      if result.is_a?(Error)
+        status_code = result.status_code
+        message = result.message
+        message_json = {message: message}.to_json
+        halt context, status_code: status_code, response: message_json
+      end
       response.status_code = 201
+      result.to_json
+    end
+
+    get "/travel_plans" do |context|
+      response = context.response
+      result = entity_factory.get_all_travels_booker
+      if result.is_a?(Error)
+        status_code = result.status_code
+        message = result.message
+        message_json = {message: message}.to_json
+        halt context, status_code: status_code, response: message_json
+      end
+      response.status_code = 200
       result.to_json
     end
   end
