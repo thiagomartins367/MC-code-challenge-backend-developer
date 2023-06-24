e1de1fa8e98978aee8f91592075f90a089a52e43
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-06-24 14:18:59 -0300

    feat(src/structs/travels_booker.cr): Implementação de serialização "JSON::Serializable"

--- src/structs/travels_booker.cr
+++ src/structs/travels_booker.cr
@@ -1,5 +1,10 @@
 struct TravelsBookerStruct
-  def initialize(@id : Int32, @travel_stops : String)
+  include JSON::Serializable # Necessário para construir JSON da struct
+
+  def initialize(
+    @id : Int32,
+    @travel_stops : String | Array(Int32),
+  )
   end
 
   getter :id
