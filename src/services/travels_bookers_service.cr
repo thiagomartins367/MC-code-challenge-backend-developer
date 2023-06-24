1695087ea4e7daa654a83622412954d46fe2913b
THIAGO MARTINS <thiago17thiago@gmail.com>
2023-06-23 23:48:52 -0300

    refactor(src/services/travels_bookers_service.cr): Refatoração geral para implementar função verificadora "verify_travels_booker_exists"

--- src/services/travels_bookers_service.cr
+++ src/services/travels_bookers_service.cr
@@ -19,6 +19,14 @@ module Api
       end
     end
 
+    private def verify_travels_booker_exists(id)
+      travels_booker = repository.get_travels_booker_by_id(id)
+      if travels_booker == nil
+        return Error.new(404, "travels_booker with id #{id} not found")
+      end
+      travels_booker
+    end
+
     def create_travels_booker(request_body)
       travel_stops = request_body["travel_stops"].as(Array)
       travel_stops_string = travel_stops.join(",")
@@ -34,30 +42,32 @@ module Api
     end
 
     def get_travels_booker_by_id(id)
-      travels_booker = repository.get_travels_booker_by_id(id)
-      return NamedTuple.new if travels_booker == nil
+      travels_booker = verify_travels_booker_exists(id)
+      return travels_booker if travels_booker.is_a?(Error)
       travels_booker = travels_booker.as(TravelsBooker)
       travels_booker_formatted = format_travels_bookers([travels_booker])
       travels_booker_formatted[0]
     end
 
     def update_travels_booker(id, request_body)
+      travels_booker = verify_travels_booker_exists(id)
+      return travels_booker if travels_booker.is_a?(Error)
+      travels_booker = travels_booker.as(TravelsBooker)
       travel_stops = request_body["travel_stops"].as(Array)
       travel_stops_string = travel_stops.join(",")
-      is_up_to_date : Bool = repository.update_travels_booker(id, travel_stops_string)
-      if !is_up_to_date
-        return Error.new(404, "travels_booker with id #{id} not found")
-      end
+      is_up_to_date : Bool = repository.update_travels_booker(travels_booker, travel_stops_string)
+      return Error.new(400, "update travels_book with id #{id} failed") if !is_up_to_date
       updated_travels_booker = TravelsBookerStruct.new(id.to_i32, travel_stops_string)
       updated_travels_booker_formatted = format_travels_bookers([updated_travels_booker])
       updated_travels_booker_formatted[0]
     end
 
     def delete_travels_booker(id)
-      is_deleted : Bool = repository.delete_travels_booker(id)
-      if !is_deleted
-        return Error.new(404, "travels_booker with id #{id} not found")
-      end
+      travels_booker = verify_travels_booker_exists(id)
+      return travels_booker if travels_booker.is_a?(Error)
+      travels_booker = travels_booker.as(TravelsBooker)
+      is_deleted : Bool = repository.delete_travels_booker(travels_booker)
+      return Error.new(400, "delete travels_book with id #{id} failed") if !is_deleted
     end
   end
 end
